2a17adbd17cfec0fea63f7690f8da5a75dc2c0e9
Takahiro-Kajima <83702146+Takahiro-Kajima@users.noreply.github.com>
2023-08-18 06:55:31 +0000

    Finish ch7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
-  def new
-    # => app/views/users/new.html.erb
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+
+    # => app/views/users/show.html.erb
   end
+
+  # GET /users/new or /signup
+  def new
+    @user = User.new
+    # => app/views.users/new.html.erb
+  end
+
+  #POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # => Success / alert-success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => GET /users/:id
+      # redirect_to user_path(@user)    # => GET /users/:id
+      # redirect_to user_path(@user.id) # => GET /users/:id
+
+    else
+      # => Failure
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
